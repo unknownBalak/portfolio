dbf95612fa9359fe3a97ed198de522450fd1572e
Shashi <shashitiwari1997@gmail.com>
2021-03-23 10:23:14 +0530

    portfolio

---
+++ src/components/Aboutme.jsx
@@ -0,0 +1,23 @@
+import React, { useState,useEffect } from "react";
+
+function Aboutme() {
+    let [i, setI] = useState(0);
+    let arr  = [
+      "I am Shashi",
+      "I am a FrontEnd Developer",
+      "I am a content creator"
+    ];
+    useEffect(() => {
+  let id = setInterval(() => {
+         console.log(i);
+        if(i===2) setI(0);
+        else setI(i+1);
+      }, 4000);
+      return () => clearInterval(id);
+    });
+    return (
+          <h1>{arr[i]} </h1>
+    )
+}
+
+export default Aboutme
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,13 @@
-import React, { useRef,useState,useEffect } from "react";
+import React from "react";
 import "./css/home.css";
  import './css/animation.css'
 import image from './resources/author_img.jpg';
 import Nav from './Nav.jsx';
 import resume from './resources/resume.pdf';
+import Aboutme from './Aboutme';
 
 
 function Home() {
-    let [i, setI] = useState(0);
-  const h1Ref = useRef();
-  let arr  = [
-    "I am Shashi",
-    "I am a FrontEnd Developer",
-    "I am a content creator"
-  ];
- useEffect(()=>{
-   let timer = setInterval(()=>{
-         if(i===3)
-         { 
-           setI(i=0);
-         }else{
-          setI(i++);
-          } 
-          h1Ref.current.textContent= arr[i];         
-   },4000)
-  return () => clearInterval(timer);
-},[])
  
   return (
     <div className="main-home-container" >
@@ -35,7 +17,7 @@ function Home() {
       <img src={image}  alt="authorPic" className='AuthorPic'/>
           </div>
           <div className="about-author">
-               <h1 ref={h1Ref} >{arr[i]}</h1>
+                <Aboutme />
                <a href={resume} download className='author-resume'> 
                 <span className="glyphicon glyphicon-cloud-download download-icon"></span>
                          DownLoad Resume
