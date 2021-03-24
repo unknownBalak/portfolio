3b15affdb9bff9cbc970e2c5094c4ab395d725de
Shashi <shashitiwari1997@gmail.com>
2021-03-24 23:00:03 +0530

    PortFolio

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
 import Home from "./components/Home";
+import About from "./components/About";
 import Footer from "./components/Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <div className="app container row">
-        <Home />
-      </div>
+      <Router>
+        <div className="app container row">
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
       <Footer />
     </>
   );
