f276b17f8841c870b6d77901817b2dc55425e5b8
Shashi <shashitiwari1997@gmail.com>
2021-04-02 15:08:10 +0530

    PortFolio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
 import Home from "./components/Home";
 import About from "./components/About";
+import Skill from "./components/Skill";
 import Footer from "./components/Footer";
+import Project from "./components/Project";
+import Contact from "./components/Contact";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
@@ -11,6 +14,15 @@ function App() {
           <Switch>
             <Route path="/about">
               <About />
+            </Route>
+            <Route path="/skill">
+              <Skill />
+            </Route>
+            <Route path="/project">
+              <Project />
+            </Route>
+            <Route path="/contact">
+              <Contact />
             </Route>
             <Route path="/">
               <Home />
---
+++ src/components/skillsData.jsx
@@ -0,0 +1,66 @@
+import java from "./resources/skillsLogo/java.svg";
+ import html from './resources/skillsLogo/html.svg';
+ import js from './resources/skillsLogo/javascript.svg';
+ import github from './resources/skillsLogo/gitHub.png';
+  import css from './resources/skillsLogo/cssIcon1.png';
+  import figma from './resources/skillsLogo/figma.svg';
+  import php from './resources/skillsLogo/php.svg';
+  import react from './resources/skillsLogo/react.svg';
+  import mongodb from './resources/skillsLogo/mongodb.svg';
+  import node from './resources/skillsLogo/node.svg';
+let skills = [
+
+    {
+        img : html,
+        description:"Advance",
+        alt :"html",
+    },
+    {
+        img : css,
+        description:"Advance",
+        alt :"css",
+
+    },
+    {
+        img : js,
+        description:"Advance",
+        alt :"js",
+
+    },
+    {
+        img : github,
+        description:"Intermediate",
+        alt :"github",
+    },
+    {
+        img : figma,
+        description:"Intermediate",
+        alt :"figma",
+    },              
+    {
+        img : react,
+        description:"Intermediate",
+        alt :"react",
+    },
+    {
+        img : java,
+        description:"Intermediate",
+        alt :"java",
+    },
+    {
+        img : node,
+        description:"Intermediate",
+        alt :"node",
+    },
+    {
+        img : mongodb,
+        description:"Intermediate",
+        alt :"mongodb",
+    },
+    {
+        img : php,
+        description:"Intermediate",
+        alt :"php",
+    },
+]
+export default skills
