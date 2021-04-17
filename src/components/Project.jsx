ac2f3057993c0caf526560b23efa82e659f465f4
Shashi <shashitiwari1997@gmail.com>
2021-04-17 08:21:48 +0530

    update with link

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -10,84 +10,84 @@ function Project() {
     <div className="projects">
       <Link to="/" className="homePage crossIcon2"></Link>
       <div className="project project1">
-        <img src={music1} alt="project1" />
+        <img src={music1} alt="project1" lazyLoading="true" />
         <div className="aboutProject">
           <h1>MusicApp</h1>
           <p>
             I have used only CSS and HTML to build this This was fun project for me. The design is
             created by @devED. click{" "}
-            <a
-              href="https://unknownbalak.github.io/musicAppClone/index.html"
+            <Link
+              to={{ pathname: "https://unknownbalak.github.io/musicAppClone/index.html" }}
               target="_blank"
               rel="noreferrer"
               style={{ color: "#07f545", fontSize: "20px", fontWeight: "bold" }}
             >
               Here
-            </a>{" "}
+            </Link>{" "}
             to preview . Here is Source{" "}
-            <a
-              href="https://github.com/unknownBalak/musicAppClone"
+            <Link
+              to={{ pathname: "https://github.com/unknownBalak/musicAppClone" }}
               target="_blank"
               rel="noreferrer"
               style={{ color: "#07f545", fontSize: "20px", fontWeight: "bold" }}
             >
               Code
-            </a>
+            </Link>
           </p>
         </div>
       </div>
       <div className="project project2">
-        <img src={absenteelist} alt="absenteelist" />
+        <img src={absenteelist} alt="absenteelist" lazyLoading="true" />
         <div className="aboutProject">
           <h1>Absentees List</h1>
           <p>
             This was my first project in JavaScript. I wrote this program because my teachers were
             facing difficulties while taking attandance of student. click{" "}
-            <a
-              href="https://unknownbalak.github.io/absenteelist/"
+            <Link
+              to={{ pathname: "https://unknownbalak.github.io/absenteelist/" }}
               target="_blank"
               rel="noreferrer"
               style={{ color: "#07f545", fontSize: "20px", fontWeight: "bold" }}
             >
               Here
-            </a>{" "}
+            </Link>{" "}
             to preview . Here is Source{" "}
-            <a
-              href="https://github.com/unknownBalak/absenteelist"
+            <Link
+              to={{ pathname: "https://github.com/unknownBalak/absenteelist" }}
               target="_blank"
               rel="noreferrer"
               style={{ color: "#07f545", fontSize: "20px", fontWeight: "bold" }}
             >
               Code
-            </a>
+            </Link>
           </p>
         </div>
       </div>
       <div className="project project3">
-        <img src={radio} alt="radio" />
+        <img src={radio} alt="radio" lazyLoading="true" />
         <div className="aboutProject">
           <h1>Radio App</h1>
           <p>
             This project tells my knowledge about API call. I called an API to display all station
             and if you want to listen music from your computer/laptop/mobile just click to station
             and play the music. . click{" "}
-            <a
-              href="radioapp.netlify.app/"
+            <Link
+              to={{ pathname: "https://radioapp.netlify.app/" }}
               target="_blank"
               rel="noreferrer"
               style={{ color: "#07f545", fontSize: "20px", fontWeight: "bold" }}
             >
               Here
-            </a>{" "}
+            </Link>{" "}
             to preview . Here is Source{" "}
-            <a
-              href="https://bit.ly/3wXfERB"
+            <Link
+              to={{ pathname: "https://bit.ly/3wXfERB" }}
               target="_blank"
               rel="noreferrer"
               style={{ color: "#07f545", fontSize: "20px", fontWeight: "bold" }}
             >
               Code
-            </a>
+            </Link>
           </p>
         </div>
       </div>
